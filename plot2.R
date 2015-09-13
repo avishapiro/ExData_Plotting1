9fbc1cca4d4ea3a604b4a8f4fa78b1ef4c6a2a5b
Avi Shapiro <avi.m.shapiro@gmail.com>
2015-09-13 19:16:42 -0400

    Initial commit

---
+++ plot2.R
@@ -0,0 +1,24 @@
+# plot2.R
+
+rawdata <- read.csv2("household_power_consumption.txt", colClasses = "character")
+cleandata <- rawdata
+
+# Filter on date range
+cleandata$Date <- as.Date(rawdata$Date, "%d/%m/%Y")
+cleandata <- cleandata[cleandata[,1] <= "2007-02-02" & cleandata[,1] >= "2007-02-01",]
+cleandata$Time <- paste(cleandata$Date, cleandata$Time)
+cleandata$Time <- strptime(cleandata$Time, "%Y-%m-%d %H:%M:%S")
+
+# Fix data types the manual way
+cleandata$Global_active_power <- as.numeric(cleandata$Global_active_power)
+cleandata$Global_reactive_power <- as.numeric(cleandata$Global_reactive_power)
+cleandata$Voltage <- as.numeric(cleandata$Voltage)
+cleandata$Global_intensity <- as.numeric(cleandata$Global_intensity)
+cleandata$Sub_metering_1 <- as.numeric(cleandata$Sub_metering_1)
+cleandata$Sub_metering_2 <- as.numeric(cleandata$Sub_metering_2)
+cleandata$Sub_metering_3 <- as.numeric(cleandata$Sub_metering_3)
+
+
+plot(cleandata$Time, cleandata$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.copy(png, file = "plot2.png")
+dev.off()
